f9ba14435d52203cdc94da76614f50aa6fbceda8
Abu <Abu@192.168.1.4>
2022-11-19 01:11:02 -0500

    this is a test push

---
+++ batch31.smart_tech/src/test/java/saturdayClass/FirstJava.java
@@ -0,0 +1,16 @@
+package saturdayClass;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		int a = 10;
+		String b = "Abu";
+		System.out.println("Hello Java");
+		System.out.println("test");
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
